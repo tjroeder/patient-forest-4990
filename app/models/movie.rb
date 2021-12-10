7c04d784a5f6f0e064225dc4f668f223d38f0bb8
Tim Roeder <timothyjroeder@gmail.com>
2021-12-09 22:47:52 -0700

    Feat: add #average_actor_age and test

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,8 @@ class Movie < ApplicationRecord
   belongs_to :studio
   has_many :movie_actors
   has_many :actors, through: :movie_actors
+
+  def average_actor_age
+    actors.average(:age)
+  end
 end
